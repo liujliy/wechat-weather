728c2ba12dad3b59eeb0dc512a10932ef2274f4c
GeneraLiuJ <32668242+liujliy@users.noreply.github.com>
2023-08-09 15:47:23 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,13 +2,11 @@
 
 set -eux
 
-CITY=Hangzhou:Xihu
 LANGUAGE="zh-CN"
-UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  https://wttr.in/$CITY?format=v2
+  https://wttr.in/Hangzhou:Xihu?format=v2&m
